6eb816e50ab17265a6470f301052e7f7aec22564
buianh <buianh5591@gmail.com>
2016-09-24 10:11:44 -0400

    a

--- Week07/Assignment/starter_code/js/exercise.js
+++ Week07/Assignment/starter_code/js/exercise.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
- $('.container').on("change", "#city-type", function{
+ $('#city-type').on("change", function () {
     event.preventDefault();
     var city = $('#city-type').val();
-
+console.log(city);
 if(city == "NYC") {
-    $('body').css("background-image","url(../images/nyc.jpg)");
+    $('body').css("background-image","url(images/nyc.jpg)");
 }
-if(city == "SF") {
-    $('body').css("background-image","url(../images/sf.jpg)");
+else if(city == "SF") {
+    $('body').css("background-image","url(images/sf.jpg)");
 }
-if(city == "LA") {
-    $('body').css("background-image","url(../images/la.jpg)");
+else if(city == "LA") {
+    $('body').css("background-image","url(images/la.jpg)");
 }
-if(city == "ATX") {
-    $('body').css("background-image","url(../images/austin.jpg)");
+else if(city == "ATX") {
+    $('body').css("background-image","url(images/austin.jpg)");
 }
-if(city == "SYD") {
-    $('body').css("background-image","url(../images/sydney.jpg)");
+else if(city == "SYD") {
+    $('body').css("background-image","url(images/sydney.jpg)");
 }
 else{
-    $('body').css("background-image","url(../images/citipix_skyline.jpg)");
+    $('body').css("background-image","url(images/citipix_skyline.jpg)");
 }
 
  });  
